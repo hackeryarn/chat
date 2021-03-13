8e704ef1ff6de104729ee8476ce0cd8fff677c3a
hackeryarn <artemchernyak@gmail.com>
2021-03-13 11:48:41 -0600

    Update heroku-setup

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -2,14 +2,13 @@
 
 (print ">>> Building system....")
 
-(require 'asdf)
+(ql:quickload :chat)
 
-(load (merge-pathnames "chat.asd" *build-dir*))
-(load (merge-pathnames "src/main.lisp" *build-dir*))
 (defun heroku-toplevel ()
   (let ((port (parse-integer (heroku-getenv "PORT"))))
     (format t "Listening on port ~A~%" port)
     (funcall (symbol-function (find-symbol 'start-app (find-package 'chat)))
              port))
   (loop (sleep 60)))
+
 (print ">>> Done building system")
