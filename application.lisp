09f4b13c16eff3c82ffdc54ad986dfb17e3017c4
hackeryarn <artemchernyak@gmail.com>
2021-03-12 18:28:30 -0600

    Simple stay alive

--- application.lisp
+++ application.lisp
@@ -3,12 +3,4 @@
 (defun initialize-application (&key port)
   (server (chat:start-app port))
 
-  (handler-case (bt:join-thread (find-if (lambda (th))
-                                         (search "hunchentoot" (bt:thread-name th))
-                                      (bt:all-threads)))
-    ;; Catch a user's C-c
-    (sb-sys:interactive-interrupt
-      () (progn
-           (format *error-output* "Aborting.~&")
-           (uiop:quit)))
-    (error (c) (format t "Woops, an unknown error occured:~&~a~&" c))))
+  (loop (sleep 60)))
