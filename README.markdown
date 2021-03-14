b5ab21892b7e7a90696e33073072e21b3fcbb61b
hackeryarn <artemchernyak@gmail.com>
2021-03-14 15:54:46 -0500

    Cleanup and notes

--- README.markdown
+++
@@ -1,5 +0,0 @@
-# Chat
-
-## Usage
-
-## Installation
--- application.lisp
+++ application.lisp
@@ -3,4 +3,20 @@
 (defun initialize-application (&key port)
   (chat:start-app port)
 
-  (loop (sleep 60)))
+  ;; Pre build binary will quit right after execution.
+  ;; To keep the server running, we bind the main thread to the "hunchentoot" thread
+  (handler-case (bt:join-thread (find-if
+                                   (lambda (th)
+                                     (search "hunchentoot" (bt:thread-name th)))
+                                   (bt:all-threads)))
+    ;; Handles C-c interupt.
+    ;; Heroku will use this to sleep the server when inactive
+    (#+sbcl sb-sys:interactive-interrupt
+          #+ccl  ccl:interrupt-signal-condition
+          #+clisp system::simple-interrupt-condition
+          #+ecl ext:interactive-interrupt
+          #+allegro excl:interrupt-signal
+          () (progn
+               (format *error-output "Aboring.~&")
+               (uiop:quit))
+        (error (c) (format t "An unknown error occured: ~&~a~&" c)))))
